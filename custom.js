539a1cc88fbb1c8390a97e0994f61f8602571296
Catherine Lu <catherineglu@gmail.com>
2013-05-03 13:08:10 +0100

    Started over. Clouds with parallax and static water

---
+++ custom.js
@@ -0,0 +1,32 @@
+/* Cloud Parallax */
+$(window).scroll(function() {
+	$("#cloud1").css({
+		top: function(index, value) {
+			return 50 - $(window).scrollTop() * .2;
+		}
+	});
+	$("#cloud2").css({
+		top: function(index, value) {
+			return 10 - $(window).scrollTop() * 1.2;
+		}
+	});
+	$("#cloud3").css({
+		top: function(index, value) {
+			return 300 - $(window).scrollTop() * 1.6;
+		}
+	});
+	$("#cloud4").css({
+		top: function(index, value) {
+			return 400 - $(window).scrollTop() * .7;
+		}
+	});
+});
+
+/* Ensure the water goes to the end of the screen */
+var waterFunction = function() {
+	$("#water-back").css("width", ($(window).width() + 20) + "px");
+	$("#water-front").css("width", ($(window).width() + 20) + "px");
+}
+
+$(document).ready(waterFunction);
+$(window).bind("resize", waterFunction);
